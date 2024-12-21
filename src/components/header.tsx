917084fc4deb543c0e12a31123131f572abffdf5
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-12-21 02:00:32 +0100

    refactor: remove Geist fonts

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">Header</h1>
+    </header>
+  )
+}
